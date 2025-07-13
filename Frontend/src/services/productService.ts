5a6dfaf1e1a623e309a8b4d9ce4645261c4f65ea
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2025-07-13 00:35:45 -0300

    feat: axios call file

---
+++ Frontend/src/services/productService.ts
@@ -0,0 +1,8 @@
+import axios from "axios"
+import { type Product } from "../types/product"
+const API_URL = "https://fakestoreapi.com/products"
+
+export const getAllProducts = async (): Promise<Product[]> => {
+    const response = await axios.get<Product[]>(API_URL)
+    return response.data
+}
