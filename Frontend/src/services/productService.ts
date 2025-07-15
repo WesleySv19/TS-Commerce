ff0bbd79497c780f28a461839503f335405d2db0
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2025-07-14 23:30:21 -0300

    feat: function for the api call

--- Frontend/src/services/productService.ts
+++ Frontend/src/services/productService.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { type Product } from "../types/product"
+import type { Product } from "../types/product"
 const API_URL = "https://fakestoreapi.com/products"
 
 export const getAllProducts = async (): Promise<Product[]> => {
