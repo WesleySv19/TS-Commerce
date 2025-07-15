01d722e24ce8bf78e574acb17ff584a7a934dcab
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2025-07-14 23:32:00 -0300

    feat: defining the project home

---
+++ Frontend/src/pages/Home/index.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import { getAllProducts } from "../../services/productService"
+import type { Product } from "../../types/product"
+import ProductCard from "../../components/ProductCard"
+
+const Home = () => {
+
+    const [products, setProducts] = useState<Product[]>([])
+
+    useEffect(() => {
+        getAllProducts()
+        .then(setProducts)
+        .catch(console.error)
+    })
+    
+  return (
+    <main className="p-8">
+        <h1 className="text-3xl font-bold text-white mb-6">Catálogo de produtos</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+    </main>
+  )
+};
+
+export default Home;
