4755658fe398d7046ccf06d1c7e52226af200907
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2025-07-17 22:26:13 -0300

    style: small style changes

--- Frontend/src/components/ProductCard/index.tsx
+++ Frontend/src/components/ProductCard/index.tsx
@@ -6,15 +6,15 @@ interface Props {
 
 const ProductCard = ({ product }: Props) => {
   return (
-    <div className="bg-gray-700 p-4 rounded-lg shadow hover:shadow-xl transition">
+    <div className="bg-gray-900 p-4 rounded-lg shadow hover:shadow-xl transition">
         <img 
         src={product.image}
         alt={product.title}
         className="h-40 w-full object-contain mb-4"
         />
 
-        <h2 className="text-lg font-semibold">{product.title}</h2>
-        <h2 className="text-green-400 font-bold mt-2">{product.price}</h2>
+        <h2 className="text-lg text-gray-200">{product.title}</h2>
+        <h2 className="text-green-400 font-bold mt-2">R$ {product.price}</h2>
     </div>
   )
 };
