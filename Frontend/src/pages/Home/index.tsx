e18647e6a2cf5250e70e27ce9cb5afd3c9c01012
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2025-07-23 22:40:16 -0300

    feat: adding loadig to the api request

--- Frontend/src/pages/Home/index.tsx
+++ Frontend/src/pages/Home/index.tsx
@@ -3,7 +3,12 @@ import { getAllProducts } from "../../services/productService"
 import type { Product } from "../../types/product"
 import ProductCard from "../../components/ProductCard"
 
+import ScrollToTopButton from "../../components/ScrollToTopButton"
+import Loader from "../../components/Loader"
+
 const Home = () => {
+
+   const [isLoading, setIsLoading] = useState(true)
 
     const [products, setProducts] = useState<Product[]>([])
 
@@ -11,16 +16,22 @@ const Home = () => {
         getAllProducts()
         .then(setProducts)
         .catch(console.error)
+        .finally(() => setIsLoading(false))
     })
     
   return (
     <main className="p-8 bg-emerald-800 font-inter">
         <h1 className="text-3xl font-bold text-primary mb-6">Catálogo de produtos</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {isLoading ? (
+            <Loader />
+        ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
             {products.map((product) => (
                 <ProductCard key={product.id} product={product} />
             ))}
-        </div>
+            </div>
+        )}
+        <ScrollToTopButton />
     </main>
   )
 };
